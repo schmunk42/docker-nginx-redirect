c899ed000b1fdac1be68e92af2299966350bfe98
Jens Giessmann <j.giessmann@herzogkommunikation.de>
2017-01-27 08:01:21 +0100

    added optional SERVER_REDIRECT_PATH Env Var to be able to define destination URL-Path for all source URLs

--- run.sh
+++ run.sh
@@ -5,6 +5,12 @@ if [ ! -n "$SERVER_REDIRECT" ] ; then
     exit 1
 fi
 
+# set redirect path from optional ENV var
+if [ ! -n "$SERVER_REDIRECT_PATH" ] ; then
+    SERVER_REDIRECT_PATH='$request_uri'
+fi
+
 sed -i "s|\${SERVER_REDIRECT}|${SERVER_REDIRECT}|" /etc/nginx/conf.d/default.conf
+sed -i "s|\${SERVER_REDIRECT_PATH}|${SERVER_REDIRECT_PATH}|" /etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
