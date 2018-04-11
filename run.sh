b408c111917e3124ce6f47a6cd855e0c7ff5bdeb
John Hossbach <john.hossbach@taillight.com>
2018-04-11 09:54:20 -0500

    Update to support changing POST redirect code

--- run.sh
+++ run.sh
@@ -15,6 +15,11 @@ if [ "$SERVER_REDIRECT_CODE" != '302' ] ; then
     SERVER_REDIRECT_CODE='301'
 fi
 
+# set POST redirect code from optional ENV var
+if [ -z "$SERVER_POST_REDIRECT_CODE" ] ; then
+    SERVER_REDIRECT_CODE='307'
+fi
+
 # set redirect path from optional ENV var
 if [ ! -n "$SERVER_REDIRECT_PATH" ] ; then
     SERVER_REDIRECT_PATH='$request_uri'
@@ -28,6 +33,7 @@ fi
 sed -i "s|\${SERVER_REDIRECT}|${SERVER_REDIRECT}|" /etc/nginx/conf.d/default.conf
 sed -i "s|\${SERVER_NAME}|${SERVER_NAME}|" /etc/nginx/conf.d/default.conf
 sed -i "s|\${SERVER_REDIRECT_CODE}|${SERVER_REDIRECT_CODE}|" /etc/nginx/conf.d/default.conf
+sed -i "s|\${SERVER_POST_REDIRECT_CODE}|${SERVER_POST_REDIRECT_CODE}|" /etc/nginx/conf.d/default.conf
 sed -i "s|\${SERVER_REDIRECT_PATH}|${SERVER_REDIRECT_PATH}|" /etc/nginx/conf.d/default.conf
 sed -i "s|\${SERVER_REDIRECT_SCHEME}|${SERVER_REDIRECT_SCHEME}|" /etc/nginx/conf.d/default.conf
 
